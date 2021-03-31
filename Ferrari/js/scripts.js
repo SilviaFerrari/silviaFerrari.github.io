80b3c047f8db2170241b524b56fc354c5b531022
silvia <40138805+SilviaFerrari@users.noreply.github.com>
2021-03-31 08:52:37 +0200

    added js directory

---
+++ Ferrari/js/scripts.js
@@ -0,0 +1,12 @@
+//play & pause audio
+var soundVettel = document.getElementById("audioVettel"); 
+var soundCar = document.getElementById("audioCar"); 
+      
+function playAudio(sound) { 
+    sound.play(); 
+} 
+      
+function pauseAudio(sound) { 
+    sound.pause(); 
+} 
+
